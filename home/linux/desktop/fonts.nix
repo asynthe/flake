229ae02be262054b5cbdb8e5bee9b7ad4cf24496
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-05 15:19:31 +0800

    fixing

--- home/linux/desktop/fonts.nix
+++ home/linux/desktop/fonts.nix
@@ -1,17 +1,18 @@
-{ pkgs, ... }: {
-
-  fonts.fontconfig.enable = true;
+{pkgs, ...}: {
+  fonts.fontconfig.enable = lib.mkForce true;
   home.packages = builtins.attrValues {
-    inherit (pkgs) 
-
+    inherit
+      (pkgs)
       source-code-pro
       font-awesome
       #iosevka-comfy.comfy
+      
       corefonts
       nerdfonts
       # Japanese
+      
       ipafont
       kochi-substitute
-
-    ;};
+      ;
+  };
 }
