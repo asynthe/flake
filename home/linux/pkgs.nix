63b22c9b5afdd44c2edd0ab3019144cf2bb18228
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-10 15:04:48 +0800

    fixing

--- home/linux/pkgs.nix
+++ home/linux/pkgs.nix
@@ -222,6 +222,9 @@
     hexedit
     hexyl # Hex editor
 
+    kicad-unstable
+    #kicad #kicad-small
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
