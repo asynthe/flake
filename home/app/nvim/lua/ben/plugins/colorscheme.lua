a8576b1b07e3b92d745c88f539a6294ed14694a2
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-10 10:58:01 +0800

    fixing

--- home/app/nvim/lua/ben/plugins/colorscheme.lua
+++ home/app/nvim/lua/ben/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
-return {
-  "bluz71/vim-nightfly-guicolors",
-  priority = 1000, -- make sure to load this before all the others
-  config = function()
-	  -- load the colorscheme here
-	  vim.cmd([[colorscheme nightfly]])
-	end,
-}
+-- [ return {
+--  "bluz71/vim-nightfly-guicolors",
+--  priority = 1000, -- make sure to load this before all the others
+--  config = function()
+--	  -- load the colorscheme here
+--	  vim.cmd([[colorscheme nightfly]])
+--	end,
+--     }
