d8a3d8067bc32fe7634f4125a28eb1301ac9bc9e
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-11 15:30:12 +0800

    fixing

--- home/dev/latex.nix
+++ home/dev/latex.nix
@@ -2,13 +2,14 @@
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
+
       pandoc
       tectonic # self-contained TeX/LaTeX engine, powered by XeTeX and TeXLive
       #tikzit
       #auto-multiple-choice
 
       # TEX SUITE
-      texlive.combined.scheme-full
+      'texlive.combined.scheme-full'
       #texlive.combined.scheme-medium
       #texlive.combined.scheme-small
       #texlive.combined.scheme-basic
