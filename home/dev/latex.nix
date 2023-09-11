846cbf971aaca4d106c4a36553de9a8665aa03fc
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-11 15:36:07 +0800

    fixing

--- home/dev/latex.nix
+++ home/dev/latex.nix
@@ -9,7 +9,7 @@
       #auto-multiple-choice
 
       # TEX SUITE
-      texlive.combined.scheme-full
+      (texlive.combined.scheme-full)
       #texlive.combined.scheme-medium
       #texlive.combined.scheme-small
       #texlive.combined.scheme-basic
