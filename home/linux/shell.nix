20cc9d632388f5a644a139c80508472876e3b288
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-17 15:11:26 +0800

    fixing

--- home/linux/shell.nix
+++ home/linux/shell.nix
@@ -12,7 +12,13 @@
   #'';
   #};
 
-  home.packages.pkgs.uutils-coreutils.override {prefix = "";} # coreutils replace with uutils
+  # Replaces coreutils with uutils.
+  # https://discourse.nixos.org/t/how-to-use-uutils-coreutils-instead-of-the-builtin-coreutils/8904/4
+  home.packages = [
+    (pkgs.uutils-coreutils.override {prefix = "";})
+  ];
+
+  # Packages
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
