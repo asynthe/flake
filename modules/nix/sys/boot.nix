4c94e1f09bf362c414eba4771ec2e4a53d6b9f3e
Benjamin Dunstan <benjadunstan@gmail.com>
2023-09-21 12:58:53 +0800

    fixing

--- modules/nix/sys/boot.nix
+++ modules/nix/sys/boot.nix
@@ -8,7 +8,7 @@
 
   # Don't suspend/hibernate when closing laptop lid
   # Options: ignore, poweroff, reboot, halt, kexec, suspend, hibernate, hybrid-sleep, suspend-then-hibernate, lock.
-  services.logind.lidSwitch = "suspend"; # or
+  services.logind.lidSwitch = "hiberate"; # or
   #services.logind.extraConfig = "HandleLidSwitch=ignore";
 
   # BOOTLOADER
