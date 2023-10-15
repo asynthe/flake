7be46a6d93a56a7c832f70aa19b837b3632dd2a0
asynthe <benjadunstan@gmail.com>
2023-10-15 21:54:49 +0800

    fix

--- modules/extra/syncthing.nix
+++ modules/extra/syncthing.nix
@@ -4,10 +4,22 @@
   pkgs,
   ...
 }: {
-  services.syncthing = {
-    enable = true;
-    systemService = true; # Run as system service.
-    user = "${username}";
+
+  # Just the package and set up myself.
+  environment.systemPackages = builtins.attrValues {
+    inherit
+      (pkgs)
+      syncthing
+      syncthingtray
+      #stc-cli
+      ;
+  };
+
+  # Enable and configure.
+  #services.syncthing = {
+    #enable = true;
+    #systemService = true; # Run as system service.
+    #user = "${username}";
     #package = ;
     #group = ""; The group to run Syncthing under, by default 'syncthing'.
     #extraFlakgs = ;
@@ -16,9 +28,9 @@
     ### SHARED FOLDERS ###
     #folders = ;
 
-  };
-  services.syncthing.relay = {
+  #};
+  #services.syncthing.relay = {
    #enable = true;
    #port = ; From 0 to 65535, default 22067. Needs to be added to networking.firewall.allowedTCPPorts
-  };
+  #};
 }
