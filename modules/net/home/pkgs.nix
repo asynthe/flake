0049b040f08497858f77b8bf61f23d36a04a348d
asynthe <benjadunstan@gmail.com>
2023-10-19 12:12:23 +0800

    fix

--- modules/net/home/pkgs.nix
+++ modules/net/home/pkgs.nix
@@ -2,6 +2,7 @@
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
+      #socat # replacement of openbsd-netcat
       bc
       qgis #qgis-ltr
       nethogs
@@ -10,7 +11,6 @@
       dnsutils # `dig` + `nslookup`
       ldns # replacement of `dig`, it provide the command `drill`
       aria2 # A lightweight multi-protocol & multi-source command-line download utility
-      socat # replacement of openbsd-netcat
       nmap # A utility for network discovery and security auditing
       ipcalc # it is a calculator for the IPv4/v6 addresses
       #inetutils
