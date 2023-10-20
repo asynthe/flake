3c0f4426114d9d3eb9b6025aadb753d5a86c405b
asynthe <benjadunstan@gmail.com>
2023-10-20 11:34:33 +0800

    fix

--- modules/snd/home/pkgs.nix
+++ modules/snd/home/pkgs.nix
@@ -2,15 +2,13 @@
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
+      losslessaudiochecker
       supercollider
       # Tools
-      
       #yabridge #yabridgectl
-      
       linthesia # Synthesia replacement
       asunder # CD ripper and encoder
       #ocenaudio #audacity
-      
       cadence # catia
       patchance # JACK patchbay GUI
       helvum # gtk patchbay for pipewire
@@ -18,45 +16,32 @@
       qpwgraph
       mp3gain
       # Audio visualizers
-      
       cli-visualizer
       cava
       # Sound volume
-      
       alsa-utils # for alsa-mixer
       pavucontrol
       pulsemixer
       ncpamixer
       # Music player
-      
       playerctl # Manage audio daemon
       ncmpcpp
       cmus
       qmmp
       # Propietary
-      
       spotify
       spotify-tui #spotify-qt
       #spotify-tray
-      
       # Tag editors
-      
       easytag
       tagger
       #puddletag
-      
       #kid3
-      
       # Soulseek
-      
       nicotine-plus #soulseekqt
-      
       # CD / DVD Tools
-      
       #cdemu-client #cdemu-daemon
-      
       #gcdemu
-      
       ;
   };
 }
