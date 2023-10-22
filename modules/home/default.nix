120b5d90560c29770434d8ef202a95a7f44e39aa
asynthe <benjadunstan@gmail.com>
2023-10-22 15:56:21 +0800

    fix

---
+++ modules/home/default.nix
@@ -0,0 +1,40 @@
+{config, ... }: {
+
+  imports = [
+
+    ./home.nix # Home Manager configuration and settings.
+    ./user # FIX LATER PLS ORGANIzE
+
+    # Home
+    ./app # Specific apps.
+    ./game # Videogames and emulators.
+    ./media # Media apps (Video, audio, dj, ...) and configs.
+    ./comms.nix
+
+    ./ime/jp.nix # Memento video player
+
+    # System tools
+    ./sys/pkgs.nix # System utilities and tools.
+
+    # Developer tools
+    ./dev/assembly.nix # Assembly
+    ./dev/tools.nix # Visual Studio Code
+    ./dev/latex.nix # LaTeX
+    ./dev/rust.nix # Python
+    ./dev/python.nix # Rust
+
+    # Network / Networking
+    ./net/pkgs.nix # Networking and packages.
+
+    # Cybersecurity
+    ./sec/pkgs.nix # Cybersecurity apps and packages.
+
+    # Audio
+    ./snd/daw.nix # Audio and sound packages.
+    ./snd/pkgs.nix # Audio apps and packages.
+    ./snd/tools.nix
+
+    # systemd Timers / Cron jobs
+    #./timer/backup.nix # systemd timer.
+  ];
+}
--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,16 +1,11 @@
-{
-  username,
-  pkgs,
-  ...
-}: {
-  imports = [
-    ../../modules/home.nix
+{ username, pkgs, ... }: {
 
-    ./usr
-    ./nix_configuration.nix
-    ./pkgs.nix
-    ./shell.nix
-  ];
+  # Fix for nixpkgs.allowUnfree = true
+  # https://discourse.nixos.org/t/unfree-packages-on-flake-based-home-manager/30231
+  nixpkgs.config = {
+    allowUnfreePredicate = _: true;
+    allowUnfree = true;
+  };
 
   home = {
     username = username;
@@ -23,4 +18,5 @@
   };
 
   programs.home-manager.enable = true; # Let Home Manager install and manage itself.
+
 }
