3e141b2ee113566791cb19a2634d958895297399
asynthe <benjadunstan@gmail.com>
2023-10-24 13:53:32 +0800

    fix

---
+++ modules/home/dev/python.nix
@@ -0,0 +1,57 @@
+{pkgs, ...}: {
+
+  home.packages = with pkgs; [
+    python312
+    (pkgs.python312.withPackages (ps:
+      with ps; [
+        pip
+	faker
+	pandas
+	scapy
+	requests
+	datetime
+	pyftpdlib
+	openpyxl # read/write Excel 2010 xlsx/xlsm/xltx/xltm files.
+	defusedxml # defusing XML bombs and other exploits
+      ]))
+  ];
+
+  #home.packages = builtins.attrValues {
+    #inherit
+      #(pkgs.python3.withPackages (ps:
+        #with ps; [
+	  #pip
+          #faker
+          #pandas
+          #scapy
+          #requests
+          #datetime
+	  #pyftpdlib
+          #openpyxl # read/write Excel 2010 xlsx/xlsm/xltx/xltm files.
+          #defusedxml # defusing XML bombs and other exploits
+        #]))
+	#;
+  #};
+  #];
+
+  # TESTING
+  #pip
+  #markdown
+  #cbeams
+  ### pywal + deps ###
+  #pywal # test
+  #scipy # for colorz
+  #pillow # for colorz
+  #colorz # not working
+  #colorthief # not working
+  ### Emacs EAF - TESTING ###
+  #pyqt6 sip qtpy epc lxml pyqt6-webengine # for eaf
+  #qrcode # eaf-file-browser
+  #pysocks # eaf-browser
+  #pymupdf # eaf-pdf-viewer
+  #pypinyin # eaf-file-manager
+  #psutil # eaf-system-monitor
+  #retry # eaf-markdown-previewer
+  #];
+  #in
+}
