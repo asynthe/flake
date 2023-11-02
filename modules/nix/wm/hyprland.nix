f2c87bc81309e9f2b424c99aa0fac832b8e05e33
asynthe <benjadunstan@gmail.com>
2023-11-02 10:43:26 +0800

    fix

--- modules/nix/wm/hyprland.nix
+++ modules/nix/wm/hyprland.nix
@@ -6,7 +6,7 @@
 }: {
   programs.hyprland = {
     enable = true;
-    #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     enableNvidiaPatches = true;
   };
 
