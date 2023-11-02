aab27191dea403ef7460e88e7282cfa1fb704ac6
asynthe <benjadunstan@gmail.com>
2023-11-02 11:14:21 +0800

    fix

--- modules/home/main/coreutils_replace.nix
+++ modules/home/main/coreutils_replace.nix
@@ -6,6 +6,8 @@
   # Replace coreutils with uutils.
   # https://discourse.nixos.org/t/how-to-use-uutils-coreutils-instead-of-the-builtin-coreutils/8904/4
   home.packages = [
-    (pkgs.uutils-coreutils.override {prefix = "";})
+
+    # Cargo from this conflicts with cargo from rustup (dev/rust.nix)
+    #(pkgs.uutils-coreutils.override {prefix = "";})
   ];
 }
