196ce69ffd84dc46c4550369f7a05e1c3e97d0b0
asynthe <benjadunstan@gmail.com>
2023-11-04 03:14:29 +0800

    fix

--- modules/nix/timer/backup_notes.nix
+++ modules/nix/timer/backup_notes.nix
@@ -4,7 +4,7 @@
   systemd.user.services."backup-notes" = {
     serviceConfig.Type = "oneshot";
     wantedBy = [ "default.target" ]; # Test with this line.
-    path = with pkgs; [ bash zip ];
+    path = with pkgs; [ bash gnutar ];
     script = ''
       bash /home/${username}/script/bash/backup/backup_notes.sh
       bash /home/${username}/script/bash/backup/backup_notes_delete_old_1_minute.sh
