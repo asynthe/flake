26ad5ceec847d73b98dd5c7bcfd5c5436d55370c
asynthe <benjadunstan@gmail.com>
2023-11-05 22:16:45 +0800

    fix

--- modules/nix/timer/backup_notes.nix
+++ modules/nix/timer/backup_notes.nix
@@ -16,7 +16,7 @@
     wantedBy = [ "timers.target" ];
     partOf = [ "test-backup.service" ];
     timerConfig = {
-      OnCalendar = "*:3/0"; # Every 3 hours.
+      OnCalendar = "*-*-* */3:00:00"; # Every 3 hours.
       Persistent = true;
     };
   };
