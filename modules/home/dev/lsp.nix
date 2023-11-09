52008d4994242b7fcbbd2c33caec5a0c426eb81b
asynthe <benjadunstan@gmail.com>
2023-11-09 23:57:50 +0800

    fix

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -2,14 +2,9 @@
 
   home.packages = builtins.attrValues {
     inherit
-      (pkgs.luajitPackages) 
-      lsp-zero-nvim
-      lua-lsp # LSP for lua.
-      ;
-      (pkgs.vimPlugins)
-      lsp-zero-nvim
-      coc-sumneko-lua;
       (pkgs) gopls;
+      #(pkgs.luajitPackages) lsp-zero-nvim lua-lsp;
+      #(pkgs.vimPlugins) lsp-zero-nvim coc-sumneko-lua;
   };
 
 }
