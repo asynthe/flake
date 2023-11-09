a0fbd0f7f741971ad3eafe093fb55f2dc64d36c5
asynthe <benjadunstan@gmail.com>
2023-11-10 00:19:43 +0800

    fix

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -4,6 +4,7 @@
     inherit (pkgs) gopls;
     inherit (pkgs.luajitPackages) lua-lsp;
     inherit (pkgs.vimPlugins) lsp-zero-nvim; #coc-sumneko-lua;
+    inherit (pkgs.python311Packages) jedi python-lsp-server;
   };
 
 }
