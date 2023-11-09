55d556c355bd824faa2875c49923678b6e40c1bd
asynthe <benjadunstan@gmail.com>
2023-11-10 00:37:36 +0800

    fix

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }: {
 
   home.packages = builtins.attrValues {
-    inherit (pkgs) gopls;
+    inherit (pkgs) 
+    gopls
+    lua-language-server # lua_ls
+    ;
     inherit (pkgs.luajitPackages) lua-lsp;
     inherit (pkgs.vimPlugins) lsp-zero-nvim; #coc-sumneko-lua;
     inherit (pkgs.python311Packages) jedi python-lsp-server;
