735833030d8ff9447e81bc7c9034e48a773836fb
asynthe <benjadunstan@gmail.com>
2023-11-10 01:16:03 +0800

    fix

--- modules/home/dev/lsp.nix
+++ modules/home/dev/lsp.nix
@@ -5,12 +5,15 @@
     # lsp-zero
     inherit (pkgs.vimPlugins) lsp-zero-nvim;
 
+    # General purpose Language Server
+    efm-langserver
+
     # Lua
     inherit (pkgs)
-    lua-language-server # lua_ls
-    stylua
+    lua-language-server # lua_ls (LSP)
+    stylua # Lua formatter
     ;
-    inherit (pkgs.luajitPackages) lua-lsp luacheck;
+    inherit (pkgs.luajitPackages) lua-lsp luacheck; # Lua linter
 
     # Python
     inherit (pkgs.python311Packages) jedi python-lsp-server;
