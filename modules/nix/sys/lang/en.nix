56e215c0224344fa91c0cbc92851a25bc2aa2617
asynthe <benjadunstan@gmail.com>
2023-11-21 09:55:03 +0800

    update

--- modules/nix/sys/lang/en.nix
+++ modules/nix/sys/lang/en.nix
@@ -10,19 +10,19 @@
       "ja_JP.EUC-JP/EUC-JP"
     ];
 
-    extraLocaleSettings = {
-      LC_ALL = "en_US.UTF-8"; # This replaces all the other ones.
-      LC_MESSAGES = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-    };
+   #extraLocaleSettings = {
+   #  LC_ALL = "en_US.UTF-8"; # This replaces all the other ones.
+   #  LC_MESSAGES = "en_US.UTF-8";
+   #  LC_TIME = "en_US.UTF-8";
+   #  LC_ADDRESS = "en_US.UTF-8";
+   #  LC_IDENTIFICATION = "en_US.UTF-8";
+   #  LC_MEASUREMENT = "en_US.UTF-8";
+   #  LC_MONETARY = "en_US.UTF-8";
+   #  LC_NAME = "en_US.UTF-8";
+   #  LC_NUMERIC = "en_US.UTF-8";
+   #  LC_PAPER = "en_US.UTF-8";
+   #  LC_TELEPHONE = "en_US.UTF-8";
+   #};
   };
 
 }
