3c62999df72da6c6e687d67a5a5a1830fb372708
asynthe <benjadunstan@gmail.com>
2023-11-21 17:47:31 +0800

    update

--- modules/nix/sys/lang/en.nix
+++ modules/nix/sys/lang/en.nix
@@ -9,8 +9,8 @@
       "ja_JP.EUC-JP/EUC-JP"
     ];
 
-   #extraLocaleSettings = {
-     #LANG = "en_US.UTF-8";
+   extraLocaleSettings = {
+     LANG = "en_US.UTF-8";
      #LC_MESSAGES = "en_US.UTF-8";
      #LC_IDENTIFICATION = "en_US.UTF-8";
      #LC_ALL = "en_US.UTF-8"; # This replaces all the other ones.
@@ -25,7 +25,8 @@
    #  LC_ADDRESS = "en_US.UTF-8/UTF-8";
    #  LC_TELEPHONE = "en_US.UTF-8/UTF-8";
    #  LC_MEASUREMENT = "en_US.UTF-8/UTF-8";
-   #};
+   };
   };
 
+  environment.systemPackages = builtins.attrValues { inherit (pkgs) glibc; };
 }
