5fac0ee4ed2d5c00dbc7132a5ba874b25cf022ec
asynthe <benjadunstan@gmail.com>
2023-11-21 15:26:19 +0800

    update

--- modules/nix/sys/lang/en.nix
+++ modules/nix/sys/lang/en.nix
@@ -1,4 +1,12 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: 
+
+let
+
+  ENGLISH = "en_US.UTF-8/UTF-8";
+  JAPANESE = "ja_JP.UTF-8/UTF-8";
+  GERMAN = "de_DE.UTF-8/UTF-8";
+
+in {
 
   time.timeZone = "Australia/Perth";
 
@@ -9,19 +17,23 @@
       "ja_JP.EUC-JP/EUC-JP"
     ];
 
-   #extraLocaleSettings = {
-   #  LC_ALL = "en_US.UTF-8"; # This replaces all the other ones.
-   #  LC_MESSAGES = "en_US.UTF-8";
-   #  LC_TIME = "en_US.UTF-8";
-   #  LC_ADDRESS = "en_US.UTF-8";
-   #  LC_IDENTIFICATION = "en_US.UTF-8";
-   #  LC_MEASUREMENT = "en_US.UTF-8";
-   #  LC_MONETARY = "en_US.UTF-8";
-   #  LC_NAME = "en_US.UTF-8";
+   extraLocaleSettings = {
+     LANG = "ENGLISH";
+     LC_MESSAGES = "ENGLISH";
+     LC_IDENTIFICATION = "ENGLISH";
+     LC_ALL = "ENGLISH"; # This replaces all the other ones.
+
+   #  LC_CTYPE = "en_US.UTF-8";
    #  LC_NUMERIC = "en_US.UTF-8";
+   #  LC_TIME = "en_US.UTF-8";
+   #  LC_COLLATE = "en_US.UTF-8";
+   #  LC_NAME = "en_US.UTF-8";
+   #  LC_MONETARY = "en_US.UTF-8";
    #  LC_PAPER = "en_US.UTF-8";
+   #  LC_ADDRESS = "en_US.UTF-8";
    #  LC_TELEPHONE = "en_US.UTF-8";
-   #};
+   #  LC_MEASUREMENT = "en_US.UTF-8";
+   };
   };
 
 }
