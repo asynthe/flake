686f4c9c4cb8906dda7b15842c01f3ae19b1209e
asynthe <benjadunstan@gmail.com>
2023-11-22 11:13:43 +0800

    update

--- modules/nix/sys/secure_boot.nix
+++ modules/nix/sys/secure_boot.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: {
 
+  imports = [
+    ../../../machines/linux/laptop/sec_boot/lanzaboote.nix
+
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs)
     sbctl niv
