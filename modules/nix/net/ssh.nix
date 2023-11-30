6b3fce2e704bcfc84bcc47690246b8853172e2f9
asynthe <benjadunstan@gmail.com>
2023-11-30 22:45:07 +0800

    fix

--- modules/nix/net/ssh.nix
+++ modules/nix/net/ssh.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{ config, pkgs, ... }: {
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
@@ -15,4 +15,10 @@
     startAgent = true; # Start ssh-agent at boot.
     agentTimeout = "null"; # How long to keep private keys in memory. Use "null" to keep forever.
   };
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+    keychain;
+  };
+
 }
