7e578863871e4e92fe1e1ebff74989111ff28d31
asynthe <benjadunstan@gmail.com>
2023-11-30 23:19:29 +0800

    fix

--- modules/nix/net/ssh.nix
+++ modules/nix/net/ssh.nix
@@ -2,7 +2,7 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    startWhenNeeded = true; # sshd will be socket-activated, it will start for each incoming connection.
+    #startWhenNeeded = true; # sshd will be socket-activated, it will start for each incoming connection.
     allowSFTP = true; # enable the STFP subsystem, use commands sftp and sshfs.
     #knownHosts = ; # ragenix?
   };
