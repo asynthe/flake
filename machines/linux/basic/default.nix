2c04b3ca0821b59436dc38fb4af03339175fb78f
Ben <benjadunstan@gmail.com>
2023-12-30 06:52:20 +0000

    fixing

--- machines/linux/basic/default.nix
+++ machines/linux/basic/default.nix
@@ -4,7 +4,7 @@
     
     ./basic.nix # Basic core server configuration.
     ./settings.nix # Nix daemon configuration.
-    ./timers.nix # Enable custom systemd services / timers.
+    ../timers.nix # Enable custom systemd services / timers.
 
     # Is this really needed on a flake?
     ./remove/hardware-configuration.nix # Include the results of the hardware scan.
