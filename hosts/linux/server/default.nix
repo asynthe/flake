6d8aa53b1530b32645c1dcb8ffa046ca60491c03
ben <benjadunstan@gmail.com>
2024-01-28 20:55:58 +0800

    owo

--- hosts/linux/server/default.nix
+++ hosts/linux/server/default.nix
@@ -9,6 +9,15 @@
     #};
   };
 
+  # MOVE TO SECRETS!
+  services.openssh = {
+    knownHosts = {
+      thinknya.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID761bx4q+wpwKmTKUZxM/m7ciQMYTBQs4qFsO4qnmLg bendunstan@skiff.com";
+      #rizuki.publicKey = "...";
+    };
+    settings.PasswordAuthentication = false;
+  };
+
   imports = [
     ./hardware.nix
 
