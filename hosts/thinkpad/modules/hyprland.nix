863f423a286b1a9a04d5a08cf4b7206928a16392
asynthe <bendunstan@protonmail.com>
2024-03-13 11:31:45 +0800

    update

--- hosts/thinkpad/modules/hyprland.nix
+++ hosts/thinkpad/modules/hyprland.nix
@@ -1,15 +1,39 @@
 { config, pkgs, ... }: {
 
-  programs.hyprland.enable = true;
-  xdg.portal = {
-    enable = true;
-    extraPortals = builtins.attrValues {
-      inherit
-        (pkgs)
-        xdg-desktop-portal-hyprland
-        xdg-desktop-portal-gtk # Required.
-        ;
+    programs.hyprland.enable = true;
+    xdg.portal = {
+        enable = true;
+        extraPortals = builtins.attrValues {
+            inherit (pkgs)
+                xdg-desktop-portal-hyprland
+                xdg-desktop-portal-gtk # Required.
+            ;
+        };
     };
-  };
 
+    environment.systemPackages = builtins.attrValues {
+        inherit (pkgs)
+
+	    imv # Image Viewer.
+            wofi # App launcher.
+	    wl-clipboard # Clipboard.
+            swww # Wallpaper.
+            mpvpaper # Video wallpaper.
+            waybar #eww-wayland # Bar.
+            swayidle # Idle daemon.
+            grim slurp # Screenshot.
+            ripdrag # Drag & Drop.
+            mako libnotify # Notification daemon.
+            wdisplays #wlr-randr #kanshi
+            brightnessctl #brillo
+            #gtklock
+	    #swaylock-effects
+            #wlogout
+	;
+        inherit (pkgs.libsForQt5) polkit-kde-agent; # Authentication Agent
+        inherit (pkgs.libsForQt5.qt5) qtwayland;
+        #inherit (pkgs.qt6) qtwayland; # Qt Wayland Support
+        #inherit (pkgs.xorg) xlsclients; # Check for apps running on xwayland
+        #inherit (pkgs.xfce) xfce4-terminal mousepad;
+    };
 }
--- hosts/thinkpad/modules/pkgs.nix
+++ hosts/thinkpad/modules/pkgs.nix
@@ -3,17 +3,15 @@
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs)
 
-      # freeimage ?
-      #imv # Image viewer.
-      imv
-
       # Programs
       alacritty kitty
       neovim
       git
       tmux lf htop
+
       librewolf ungoogled-chromium
       #tor-browser
+
       pavucontrol
       wget curl
       killall
@@ -23,41 +21,19 @@
       qbittorrent
       pcmanfm
 
-      # Pass
-      wl-clipboard
-      pass-wayland
+      # CLI
+      ffmpeg
+      imagemagick
 
-      # Wayland
-      wofi # App launcher.
-      swww # Wallpaper.
-      mpvpaper # Video wallpaper.
-      grim slurp # Screenshot.
-      ripdrag
+      # Pass
+      pass-wayland
 
       # Music
       cmus
       mpd
       ncmpcpp
       spotify
-
-      waybar # Bar
-      swayidle # Idle daemon
-      mako libnotify # Notification daemon
-      wdisplays #wlr-randr #kanshi
-      brightnessctl #brillo
-      # Disabled
-      # Lock Screen
-      #gtklock
-      #swaylock-effects
-      #wlogout
-      #eww-wayland # status bar
       ;
-
-    inherit (pkgs.libsForQt5) polkit-kde-agent; # Authentication Agent
-    inherit (pkgs.libsForQt5.qt5) qtwayland;
-    #inherit (pkgs.qt6) qtwayland; # Qt Wayland Support
-    #inherit (pkgs.xorg) xlsclients; # Check for apps running on xwayland
-    #inherit (pkgs.xfce) xfce4-terminal mousepad;
   };
 
 }
