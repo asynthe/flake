621cf821b9d6e9eabe5531609f8aef1b80a50b41
asynthe <bendunstan@protonmail.com>
2024-03-17 04:15:20 +0800

    update

--- home/ben/modules/app/lf/default.nix
+++ home/ben/modules/app/lf/default.nix
@@ -1,13 +1,8 @@
 {
-
-    # I have separated the scripts and the configuration, as having the
-    # scripts in the `let` of the main file would make it long.
-
     imports = [
 
       ./lf.nix
       ./lf_scripts.nix
 
     ];
-
 }
