6c32f19dd46cb298a9b78866110036d64aaf598c
missingno <bendunstan@protonmail.com>
2024-04-18 15:27:29 +0800

    update

--- home/missingno/modules.nix
+++ home/missingno/modules.nix
@@ -7,7 +7,6 @@
 
         # Apps
 	../modules/app/alacritty.nix
-        ../modules/app/android.nix # adb + waydroid.
         #../modules/app/bash.nix # bash shell.
 	../modules/app/bat.nix
 	../modules/app/direnv.nix
@@ -16,17 +15,14 @@
         ../modules/app/git.nix
         ../modules/app/gpg.nix
         ../modules/app/lf.nix
-        #../modules/app/mako.nix
 	#../modules/app/mpd.nix
 	../modules/app/mpv.nix
 	#../modules/app/obs.nix
 	#../modules/app/social.nix
-        ../modules/app/ssh.nix
+        #../modules/app/ssh.nix
         ../modules/app/starship.nix
         ../modules/app/tmux.nix
-	../modules/app/vscode.nix
-	../modules/app/wayfire.nix
-        #../modules/app/wofi.nix
+	#../modules/app/vscode.nix
         ../modules/app/yazi.nix
         ../modules/app/zathura.nix
         ../modules/app/zoxide.nix
