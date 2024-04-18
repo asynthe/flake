a90952537bda74788299359b6a0277f8cd3dcfc4
missingno <bendunstan@protonmail.com>
2024-04-18 15:20:33 +0800

    update

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -3,6 +3,11 @@
     wsl = {
         enable = true;
 	defaultUser = "nixos";
+	startMenuLaunchers = true;
+	#wslConf = {
+	    #automount.root = "/home/${user}/win";
+	    #boot.command = "";
+	#};
     };
 
     environment.systemPackages = builtins.attrValues {
