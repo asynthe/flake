e93499a06ac06cbef3b60da2e2fff2a5cde59747
asynthe <bendunstan@protonmail.com>
2024-05-12 21:58:14 +0900

    update

--- home/missingno/modules.nix
+++ home/missingno/modules.nix
@@ -2,27 +2,27 @@
     imports = [
         
         ../modules/pkgs_minimal.nix
-	../modules/sec/pkgs.nix
-	#../modules/sec/home-security-box.nix # Kali ?
+	    ../modules/sec/pkgs.nix
+	    #../modules/sec/home-security-box.nix # Kali ?
 
         # Apps
-	../modules/app/alacritty.nix
+	    ../modules/app/alacritty.nix
         #../modules/app/bash.nix # bash shell.
-	../modules/app/bat.nix
-	../modules/app/direnv.nix
-	#../modules/app/emacs.nix
-	../modules/app/eza.nix
+	    ../modules/app/bat.nix
+	    ../modules/app/direnv.nix
+	    #../modules/app/emacs.nix
+	    ../modules/app/eza.nix
         ../modules/app/git.nix
         ../modules/app/gpg.nix
         ../modules/app/lf.nix
-	#../modules/app/mpd.nix
-	../modules/app/mpv.nix
-	#../modules/app/obs.nix
-	#../modules/app/social.nix
+	    #../modules/app/mpd.nix
+	    ../modules/app/mpv.nix
+	    #../modules/app/obs.nix
+	    #../modules/app/social.nix
         #../modules/app/ssh.nix
         ../modules/app/starship.nix
         ../modules/app/tmux.nix
-	#../modules/app/vscode.nix
+	    #../modules/app/vscode.nix
         ../modules/app/yazi.nix
         ../modules/app/zathura.nix
         ../modules/app/zoxide.nix
@@ -30,8 +30,8 @@
 
         # System
         ../modules/sys/settings.nix
-	../modules/sys/theme.nix
-	#../modules/sys/uutils-replace.nix
-	../modules/sys/var.nix
+	    ../modules/sys/theme.nix
+	    #../modules/sys/uutils-replace.nix
+	    ../modules/sys/var.nix
     ];
 }
