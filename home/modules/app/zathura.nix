d5616471340ee3c3d86a277426afc3af499702c4
asynthe <bendunstan@protonmail.com>
2024-07-08 21:57:12 +0800

    update

--- home/modules/app/zathura.nix
+++ home/modules/app/zathura.nix
@@ -4,16 +4,47 @@
         sessionVariables.READER = "zathura";
         shellAliases = {
 	        pdf = "zathura";
-	        book = "${pkgs.fd}/bin/fd . ~/sync/archive/book --type f -e 'pdf' -e 'epub' | ${pkgs.skim}/bin/sk | xargs ${pkgs.zathura}/bin/zathura";
         };
     };
+
+    stylix.targets.zathura.enable = lib.mkForce false;
 
     programs.zathura = {
         enable = true;
 	    extraConfig = ''
-	      set guioption ""
-	      #set recolor true
-	      set selection-clipboard clipboard
+# Settings
+set guioptions ""
+set selection-clipboard clipboard
+set adjust-open "best-fit"
+set pages-per-row 1
+set zoom-min 10
+set render-loading "false"
+
+# Scroll
+set scroll-page-aware "true"
+set smooth-scroll "true"
+set scroll-full-overlap 0.01
+#set scroll-step 100
+set scroll-step 50
+
+# Keybinds
+unmap f
+map f toggle_fullscreen
+map [fullscreen] f toggle_fullscreen
+
+# Start in dark mode
+set recolor true
+set recolor-lightcolor "#E0E0E0" #06
+set recolor-reverse-video "true"
+set recolor-keephue "true"
+
+# UI
+set font "Iosevka Nerd Font 14"
+set default-bg "#000000" #00
+set default-fg "#F7F7F6" #01
+
+set statusbar-fg "#B0B0B0" #04
+set statusbar-bg "#202020" #01
 	    '';
     };
 }
