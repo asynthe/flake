b3a220e80c7e8782e8e6ec639a5e9851c8be2820
asynthe <bendunstan@protonmail.com>
2024-07-16 22:07:29 +0800

    update

--- home/modules/wayland/hyprland/configuration.nix
+++ home/modules/wayland/hyprland/configuration.nix
@@ -55,8 +55,8 @@ in {
 
         device = {
             # Old Razer Hyperspeed X mouse
-            name = "razer-razer-basilisk-x-hyperspeed-1";
-            #name = "razer-razer-basilisk-x-hyperspeed";
+            #name = "razer-razer-basilisk-x-hyperspeed-1";
+            name = "razer-razer-basilisk-x-hyperspeed";
             sensitivity = "-0.8"; # from -1.0 to 1.0, default 0.0
             accel_profile = "flat";
         };
